81c365f46517f6921dda11fc445f399474023917
shintan777 <43470116+shintan777@users.noreply.github.com>
2018-09-22 23:25:28 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class pattern
+{
+	public static void main(String args[])
+	{	System.out.println("Enter no. of lines of pattern");
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int j,k,l;
+		for (l=0;l<n;l++)
+		{
+			for (k=n;k>l;k--)
+			{System.out.print(" ");
+			}
+		
+			for (j=0;j<=l;j++)
+			{System.out.print("* ");
+			}
+			System.out.print("\n");
+		}
+		
+	}
+	
+
+
+}
